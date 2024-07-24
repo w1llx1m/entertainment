97e1890cd12252b77c6295dd6117abeed356c3a9
w1llx1m <williamsilva.monteiro28@gmail.com>
2024-07-23 21:27:58 -0300

    climate api

--- movies/views.py
+++ movies/views.py
@@ -3,5 +3,14 @@ import requests as rqt
 import json
 
 def home(request):
-    context = {}
+    context = climate()
+    Roundedret = round(context['current']['temperature_2m'])
+    context['current']['temperature_2m'] = str(Roundedret)
     return render(request, 'home.html', context)
+
+
+def climate():
+    ret = rqt.get("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m")
+    jsonret = json.loads(ret.content.decode('utf-8'))
+    print(jsonret)
+    return jsonret
